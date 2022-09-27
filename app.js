a20defeed6e129c2d7b9b99ce95634ea56031229
Aleph Null <likhonhasankomool@gmail.com>
2022-09-27 14:51:43 -0400

    can add multiple projects and choose to feature a project

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 const inquirer = require('inquirer');
 
 
-/*const fs = require('fs');
+const fs = require('fs');
 const generatePage = require('./src/page-template');
 
-const pageHTML = generatePage(username, github);
+//const pageHTML = generatePage(username, github);
 
-fs.writeFile('./index.html',pageHTML,err =>
-{
-    if(err) throw new Error(err);
+//fs.writeFile('./index.html',pageHTML,err =>
+//{
+    //if(err) throw new Error(err);
 
-    console.log("Portfolio complete, check index.html to see output");
-});*/
+    //console.log("Portfolio complete, check index.html to see output");
+//});
 
 const promptUser = () => {
    return inquirer
@@ -176,5 +176,11 @@ const promptProject = portfolioData => {
 promptUser()
 .then(promptProject)
 .then(portfolioData =>{
-    console.log(portfolioData);
+    const pageHTML = generatePage(portfolioData);
+
+    fs.writeFile('./index.html', pageHTML, err => {
+       if (err) throw new Error(err);
+
+       console.log('Page created! Check out index.html in this directory to see it!');
+     });
 });
